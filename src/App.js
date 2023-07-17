02c5b3044c3283a6a9ce81e8be026791c9c6d200
Rouzbeh Rahbar <rouzbeh.rahbar4@gmail.com>
2023-07-18 01:25:22 +0200

    building the layout

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Travel 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>🧳 You have X on your list, and you already packed X (x%)</em>
+    </footer>
+  );
+}

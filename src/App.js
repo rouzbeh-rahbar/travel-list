61e3cf594ffd11870b05be6bf73ce42d2f0d2684
Rouzbeh Rahbar <rouzbeh.rahbar4@gmail.com>
2023-07-18 02:06:41 +0200

    rendering the items list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+// This array with a few objects.
+// Each objects describesone item to be packed
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
 export default function App() {
   return (
     <div className="app">
@@ -21,7 +28,26 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
